185e57a57e608893479a336bb0dc31a3c40bcfe6
BLUECOMET <morgan94.dev@gmail.com>
2024-02-18 14:22:57 -0500

    move repository from organization to personal

---
+++ Streamlit_Frontend/Hello.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Diet Recommendation System! 👋")
+
+st.sidebar.success("Select a recommendation app.")
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    You can find more details and the whole project on my [repo](https://github.com/zakaria-narjis/Diet-Recommendation-System).
+    """
+)
